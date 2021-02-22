8455a630f864684b56d1c2448ff3cbeef55007ec
PDRB <rodriguezbaumann2@gmail.com>
2021-02-22 17:32:14 -0300

    Terminados todos los desafios, agregado de looking good

--- Desafio1.py
+++ Desafio1.py
@@ -4,13 +4,11 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 import json
 import os
-
 from pip._vendor import requests
 import pandas as pd
 
 
-def buscameElResponse(param, url_api, url_entrada):
-    # Use a breakpoint in the code line below to debug your script.
+def desafio1(param, url_api, url_entrada):
     x = requests.get(url_api)
     json_data = json.loads(x.text)
     print(x)
@@ -19,10 +17,9 @@ def buscameElResponse(param, url_api, url_entrada):
     print(param)
 
 
-def mostrameeldataframe(param):
-    with open(os.getcwd()+'/recursos/Sellers.json') as json_file:
+def desafio2(param):
+    with open(os.getcwd() + '/recursos/Sellers.json') as json_file:
         data = json.load(json_file)
-
         rows_positive = []
         rows_neutral = []
         rows_negative = []
@@ -32,35 +29,35 @@ def mostrameeldataframe(param):
             siteid = [item['body']["site_id"]]
             nickname = item['body']["nickname"]
         seller_reputation = seller_reputation_deglosed["total"]
-        if seller_reputation >= 1: rows_positive.append([sellerid, siteid, nickname, seller_reputation])
+        if seller_reputation >= 1:  rows_positive.append([sellerid, siteid, nickname, seller_reputation])
+
         if seller_reputation <= -1:
             rows_negative.append([sellerid, siteid, nickname, seller_reputation])
         else:
             rows_neutral.append([sellerid, siteid, nickname, seller_reputation])
 
     df = pd.DataFrame(rows_neutral, columns=['id', 'site_id', 'nickname', 'total'])
-    df.to_csv(os.getcwd()+'/searcher/json/2020/02/neutral/neutral.csv')
+    df.to_csv(os.getcwd() + '/searcher/json/2020/02/neutral/neutral.csv')
     df = pd.DataFrame(rows_negative, columns=['id', 'site_id', 'nickname', 'total'])
-    df.to_csv(os.getcwd()+'/searcher/json/2020/02/negative/negative.csv')
+    df.to_csv(os.getcwd() + '/searcher/json/2020/02/negative/negative.csv')
     df = pd.DataFrame(rows_positive, columns=['id', 'site_id', 'nickname', 'total'])
-    df.to_csv(os.getcwd()+'/searcher/json/2020/02/positive/positive.csv')
+    df.to_csv(os.getcwd() + '/searcher/json/2020/02/positive/positive.csv')
     print(df)
     print(param)
 
 
-# Press the green button in the gutter to run the script.
-def buscaymostranuevodatafram(param, url_entrada, url_salida):
+def desafio4(param, url_entrada, url_salida):
     with open(url_entrada) as json_file:
         data = json.load(json_file)
         rows = []
         i = 0
         for item in data['results']:
-            i = i+1
+            i = i + 1
             rowid = i
             itemid = item['id']
-            availableQuantity = item['available_quantity']
-            soldQuantity = item['sold_quantity']
-            rows.append([rowid, itemid, availableQuantity, soldQuantity])
+            availablequantity = item['available_quantity']
+            soldquantity = item['sold_quantity']
+            rows.append([rowid, itemid, availablequantity, soldquantity])
 
     df = pd.DataFrame(rows, columns=['rowid', 'itemId', 'availableQuantity', 'soldQuantity'])
     df.to_csv(url_salida, index=False)
@@ -70,31 +67,72 @@ def buscaymostranuevodatafram(param, url_entrada, url_salida):
     pass
 
 
-def obtenerdataframe(url_entrada ,columnas):
-    column_names = ['rowid', 'itemId', 'availableQuantity', 'soldQuantity']
-    iris = pd.read_csv(url_entrada, names=columnas)
-    return iris
+def desafio3(param, url_entrada1, url_entrada2):
 
-
-def vamosajoinear(param, url_entrada1, url_entrada2):
     print(pd.read_csv(url_entrada2))
     dataframe1 = pd.read_csv(url_entrada2)
     print(pd.read_csv(url_entrada1))
     dataframe2 = pd.read_csv(url_entrada1)
-    dataframfinal = pd.merge(dataframe1, dataframe2,how='inner', on=['itemId'])
+    dataframfinal = pd.merge(dataframe1, dataframe2, how='inner', on=['itemId'])
     dataframfinal = dataframfinal.filter(["itemId", "visits", "soldQuantity"])
-    print('antes del filtro'+dataframfinal)
-    dataframfinal= dataframfinal[(dataframfinal.soldQuantity > 1)]
-    print('despues del filtro'+dataframfinal)
+    print('antes del filtro')
+    print(dataframfinal)
+    dataframfinal = dataframfinal[(dataframfinal.soldQuantity > 1)]
+    print('despues del filtro')
+    print(dataframfinal)
+    print(param)
+    dataframfinal.to_csv(os.getcwd() + '/searcher/json/2020/02/resultados_join_visitas.csv', index=False)
+    pass
+
+
+def desafio5(param, url_entrada):
+    dataframe1 = pd.read_csv(url_entrada)
+    dataframe1['conversionRate'] = dataframe1["soldQuantity"] / dataframe1["visits"]
+    dataframe_final = dataframe1.sort_values('conversionRate', ascending=False)
+    dataframe_final['conversionRanking'] = range(1, 1 + len(dataframe_final))
+    print(dataframe_final)
     print(param)
     pass
 
 
+def desafio6(param, url_entrada):
+    dataframe = pd.read_csv(url_entrada)
+    dataframe['porcentajeStock'] = (dataframe['availableQuantity'] * 100) / (
+            dataframe['availableQuantity'] + dataframe['soldQuantity'])
+    dataframefiltrado = dataframe.filter(['itemId', 'availableQuantity', 'porcentajeStock'])
+    dataframefiltrado = dataframefiltrado.sort_values('porcentajeStock', ascending=False)
+    print(dataframefiltrado)
+    pass
+
+
 if __name__ == '__main__':
-    #   buscameElResponse('Lo logramos pasamos el primer desafio',
-    # 'https://api.mercadolibre.com/sites/MLA/search?category=MLA1000',
-    # 'os.getcwd()+'/searcher/json/2020/02/MLA100.json')
-    #    mostrameeldataframe('Lo logramos pasamos el segundo desafio')
-        buscaymostranuevodatafram('Con python logramos el tercer desafio',os.getcwd()+'/recursos/MPE1004.json',os.getcwd()+'/searcher/json/2020/02/neutral/neutral.csv')
-        vamosajoinear(' logramos el cuarto desafio',                  os.getcwd()+'/searcher/json/2020/02/neutral/neutral.csv',
-                 os.getcwd()+'/recursos/visits.csv')
+    val = input("Bienvenido a la demostracion de challenge seleccione la opcion: \n"
+                "1 desafio de api                   -----\n"
+                "2 desafio de deserializacion       -----\n"
+                "3 desafio de parseo de dataframe   -----\n"
+                "4 desafio de join                  -----\n"
+                "5 desafio de metricas              -----\n"
+                "6 desafio de metricas 2            -----\n")
+    print(val)
+    if int(val) == 1:    desafio1('Lo logramos pasamos el primer desafio',
+                      'https://api.mercadolibre.com/sites/MLA/search?category=MLA1000',
+                        os.getcwd() + '/searcher/json/2020/02/MLA100.json')
+    val = input("Perfecto, continuemos con las pruebas o termine el proceso: \n")
+    if int(val) == 2:
+             desafio2('Lo logramos pasamos el segundo desafio')
+    val = input("Perfecto, continuemos con las pruebas o termine el proceso: \n")
+    if int(val) == 3:
+             desafio3(' logramos el cuarto desafio', os.getcwd() + '/searcher/json/2020/02/neutral/neutral.csv',
+             os.getcwd() + '/recursos/visits.csv')
+             val = input("Perfecto, continuemos con las pruebas o termine el proceso: \n")
+    if int(val) == 4:
+             desafio4('Con python logramos el tercer desafio', os.getcwd() + '/recursos/MPE1004.json',
+             os.getcwd() + '/searcher/json/2020/02/neutral/neutral.csv')
+             val = input("Perfecto, continuemos con las pruebas o termine el proceso: \n")
+    if int(val) == 5:
+            desafio5(' logramos el cuarto desafio', os.getcwd() + '/searcher/json/2020/02/resultados_join_visitas.csv')
+            val = input("Perfecto, continuemos con las pruebas o termine el proceso: \n")
+    if int(val) == 6:
+             desafio6(' logramos el septimo desafio', os.getcwd() + '/searcher/json/2020/02/neutral/neutral.csv')
+             val = input("Perfecto, continuemos con las pruebas o termine el proceso: \n")
+
